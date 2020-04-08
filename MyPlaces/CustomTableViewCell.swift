4bcfd6116a13bbf057fc2848c17c96824e918285
a-trefilova <atrefilova97@icloud.com>
2020-04-08 15:18:43 +0300

    Adding custom cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Константин Сабицкий on 08.04.2020.
+//  Copyright © 2020 Константин Сабицкий. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBOutlet weak var typeLabel: UILabel!
+}
